596062b7ac9eb264e435afcf93405a0e9a85cac3
Edwar Mauricio Mayorga Llanos <emmllanitos@hotmail.com>
2022-11-07 12:05:23 -0500

    Se agrega Css personalizado

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,3 +1,20 @@
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
 export function Login() {
-  return <div>login</div>;
+  return (
+    <div className="containerForms">
+      login
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Correo electronico</Form.Label>
+          <Form.Control type="email" placeholder="correo@dominio.com" />
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Pass word" />
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
